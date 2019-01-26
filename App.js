2ea74032d0fb96eeb4021b2adec81b527114d146
Brandon In <brandonin@me.com>
2019-01-25 16:54:32 -0800

    added the types of triangle this is

--- App.js
+++ App.js
@@ -2,6 +2,66 @@ import React from 'react';
 import { Image, TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    sides: {
+      first: "2",
+      second: "3",
+      third: "2",
+    },
+    triangleTypes: []
+  }
+
+  componentDidMount = () => {
+    this.determineTriangleType();
+  }
+
+
+  handleChange = async (key, value) => {
+    let sides = {...this.state.sides};
+    sides[key] = value;
+    await this.setState({sides});
+    this.determineTriangleType();
+  }
+
+  determineTriangleType = () => {
+    const {first, second, third} = this.state.sides;
+    let hypotenuse = -Infinity;
+    let hypotenuseKey;
+    for (const [key, value] of Object.entries(this.state.sides)) {
+      if (value > hypotenuse) {
+        hypotenuse = value;
+        hypotenuseKey = key;
+      }
+    }
+    let [side1, side2] = assignSides(this.state.sides);
+    let triangleTypes = [];
+
+    // triangle by sides
+    if (hypotenuse === side1 && side1 === side2) triangleTypes.push("equilateral", "equilangular");
+    else if (hypotenuse === side1 || hypotenuse === side2 || side1 === side2) triangleTypes.push("isosceles");
+    else triangleTypes.push('scalene');
+
+    // triangle by angles
+    if (Math.pow(hypotenuse, 2) == Math.pow(side1, 2) + Math.pow(side2, 2)) triangleTypes.push("right");
+    if (Math.pow(hypotenuse, 2) < Math.pow(side1, 2) + Math.pow(side2, 2)) triangleTypes.push("acute");
+    if (Math.pow(hypotenuse, 2) > Math.pow(side1, 2) + Math.pow(side2, 2)) triangleTypes.push("obtuse");
+
+    this.setState({triangleTypes});
+    function assignSides(object) {
+      for (const [key, value] of Object.entries(object)) {
+        if (key === hypotenuseKey) {
+          continue;
+        }
+        if (!side1) {
+          side1 = value || 0;
+        } else {
+          side2 = value || 0;
+        }
+      }
+      return [side1, side2];
+    }
+  }
+
   render() {
     const tsLogo = {
         uri: 'https://tradeshift.com/wp-content/themes/Tradeshift/img/brand/logo-black.png',
@@ -12,7 +72,13 @@ export default class App extends React.Component {
           <Image source={tsLogo} style={{width: 332, height: 40}}/>
         </View>
         <View style={styles.textContainer}>
-            <TextInput placeholder="hello"></TextInput>
+            <TextInput keyboardType="numeric" value={this.state.sides.first} placeholder="hello" onChangeText={(value) => this.handleChange('first', value)}></TextInput>
+            <TextInput keyboardType="numeric" value={this.state.sides.second} placeholder="hello" onChangeText={(value) => this.handleChange('second', value)}></TextInput>
+            <TextInput keyboardType="numeric" value={this.state.sides.third} placeholder="hello" onChangeText={(value) => this.handleChange('third', value)}></TextInput>
+            <Text>Types of Traingle this is:</Text>
+            {this.state.triangleTypes.map((value, key) => {
+              return <Text key={key}>{value}</Text>;
+            })}
         </View>
       </View>
     );
